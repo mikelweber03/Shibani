52226799079ea8eaa8f02ae90f6bb086cb0fd5ce
Giuseppe Napoli <napoligiuseppe@web.de>
2023-01-18 17:26:16 +0100

    Made some new Scenes to test Performance

--- Assets/Scripts/turret.cs
+++ Assets/Scripts/turret.cs
@@ -24,6 +24,23 @@ public class turret : MonoBehaviour
 
     public int maxHealth;
 
+    public Transform target; //where we want to shoot(player? mouse?)
+    public Transform weaponMuzzle; //The empty game object which will be our weapon muzzle to shoot from
+    public GameObject bullet; //Your set-up prefab
+    public float fireRate = 3000f; //Fire every 3 seconds
+    public float shootingPower = 20f; //force of projection
+
+
+    private float shootingTime; //local to store last time we shot so we can make sure its done every 3s
+
+    public int health = 100;
+    //public GameObject deathEffect;
+
+
+
+    //private float position;
+    //private Transform positionPlayer;
+
     private int count = 0;
     private Vector3 start;
     private VisualEffect enemyHit;
@@ -40,6 +57,8 @@ public class turret : MonoBehaviour
         Awareness = gameObject.GetComponentInChildren<SphereCollider>();
         enemyHit = GetComponentInChildren<VisualEffect>();
         enemyDeath = GetComponentInChildren<VisualEffect>();
+        
+        //position = this.transform.position.x;
     }
 
     // Update is called once per frame
@@ -47,12 +66,25 @@ public class turret : MonoBehaviour
     {
         CheckPlayerDistance();
         GotHit();
-        ShootBullet();
+        //ShootBullet();
         if (bisFiring)
         {
-            //TurretFire();
+            Fire();
         }
      }
+
+
+    private void Fire()
+    {
+        if (Time.time > shootingTime)
+        {
+            shootingTime = Time.time + fireRate / 1000; //set the local var. to current time of shooting
+            Vector2 myPos = new Vector2(weaponMuzzle.position.x, weaponMuzzle.position.y); //our curr position is where our muzzle points
+            GameObject projectile = Instantiate(bullet, myPos, Quaternion.identity); //create our bullet
+            Vector2 direction = myPos - (Vector2)target.position; //get the direction to the target
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * shootingPower; //shoot the bullet
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
