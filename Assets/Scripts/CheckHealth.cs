b4917e2c400dc0b84bc071a4e87b7bf18be22c30
Benno Hobrecht <benno.hobrecht@games-academy.de>
2023-02-06 17:40:17 +0100

    added new prefabs

--- Assets/Scripts/CheckHealth.cs
+++ Assets/Scripts/CheckHealth.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
-using UnityEngine.Rendering.PostProcessing;
+//using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class CheckHealth : MonoBehaviour
